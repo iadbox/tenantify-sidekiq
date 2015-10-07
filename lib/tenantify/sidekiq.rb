3cb7d53098846fdc40416ff5d7b9ebed5a6ca9b5
Jaime Cabot Campins <jcabot@gmail.com>
2015-10-07 19:03:26 +0530

    Initial commit

---
+++ lib/tenantify/sidekiq.rb
@@ -0,0 +1,7 @@
+require "tenantify/sidekiq/version"
+
+module Tenantify
+  module Sidekiq
+    # Your code goes here...
+  end
+end
---
+++ lib/tenantify/sidekiq/version.rb
@@ -0,0 +1,5 @@
+module Tenantify
+  module Sidekiq
+    VERSION = "0.0.1"
+  end
+end
